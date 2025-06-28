ae8479d3abe8b865e84a9400d7b73f7d31fdcb39
student-kiitcollege <jenasourav2001@gmail.com>
2025-06-28 11:10:12 +0530

    Updated Code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,14 +6,8 @@ import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
 import { v2 as cloudinary } from 'cloudinary'
 import stripe from "stripe";
-import razorpay from 'razorpay';
 
-// Gateway Initialize
-const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-})
+
 
 // API to register user
 const registerUser = async (req, res) => {
